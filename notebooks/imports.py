16d6aa1f7f894f3f8bbc0764d81822e1cf26121e
vaibrainium <vaibhavt459@gmail.com>
2025-02-17 22:37:32 +0000

    feat: reparameterization complete

--- notebooks/imports.py
+++ notebooks/imports.py
@@ -1,9 +1,9 @@
-
-from pathlib import Path
+import warnings
+# suppress warnings
+warnings.filterwarnings("ignore")
 
 import numpy as np
 import pandas as pd
-
 pd.options.display.max_rows = 999
 pd.options.display.max_columns = 999
 pd.set_option("display.max_columns", None)
