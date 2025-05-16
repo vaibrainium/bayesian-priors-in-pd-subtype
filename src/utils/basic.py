469ab686fbe6aba5bd63d8cc1f93899c440f3410
vaibrainium <vaibhavt459@gmail.com>
2025-05-16 22:28:31 +0000

    chore: code style improved

--- src/utils/basic.py
+++ src/utils/basic.py
@@ -3,20 +3,17 @@ from pathlib import Path
 import dill
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from matplotlib import style
-
 BASE_DIR = Path(__file__).parent
 
-def save_model(model: dict, name: str, dir: str = BASE_DIR / "src" / "models/"):    
+def save_model(model: dict, name: str, dir: str = BASE_DIR / "src" / "models/"):
     # print(dir.resolve().exists())
-    # file_dir = Path(dir) if dir else 
-    file = Path(dir) / f"{name}.pkl" 
+    # file_dir = Path(dir) if dir else
+    file = Path(dir) / f"{name}.pkl"
     with open(file, "wb") as f:
         dill.dump(model, f)
-        
+
 def load_model(name: str, dir: str = BASE_DIR / "src" / "models/"):
-    file = Path(dir) / f"{name}.pkl" 
+    file = Path(dir) / f"{name}.pkl"
     with open(file, "rb") as f:
         model = dill.load(f)
     return model
