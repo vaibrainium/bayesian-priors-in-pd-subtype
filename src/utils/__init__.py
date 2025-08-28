c0959c59e21feea979fcfabb32b8c7ec5c7e1273
vaibrainium <vaibhavt459@gmail.com>
2025-08-28 01:10:22 +0000

    feat: data compilation and categorization complete

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,6 @@
+class basic_utils:
+    from .basic import load_model, raise_error, save_model
+
 
 class plot_utils:
     from .plotting import (
@@ -6,22 +9,26 @@ class plot_utils:
         figure_with_cbar_by_height,
         grid_by_height,
         grid_by_width,
-        plot_scatter,
-        plot_line,
         plot_errorbar,
+        plot_line,
+        plot_scatter,
         plot_x_errorbar,
     )
+
 
 class pmf_utils:
     from .pmf_utils import (
         fit_psychometric_function,
-        get_psychometric_data,
-        get_chronometric_data,
         get_accuracy_data,
+        get_chronometric_data,
+        get_psychometric_data,
     )
+
 
 # Create a limited interface for glm_hmm_utils
 class glm_hmm_utils:
-    from .glm_hmm_utils import global_fit, session_wise_fit_cv, session_wise_fit
+    from .glm_hmm_utils import global_fit, session_wise_fit
+    from .glm_hmm_utils_cv import session_wise_fit_cv
 
-__all__ = ["plot_utils", "pmf_utils", "glm_hmm_utils"]
+
+__all__ = ["basic_utils", "glm_hmm_utils", "plot_utils", "pmf_utils"]
--- src/utils/basic.py
+++ src/utils/basic.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 import dill
-import numpy as np
-import pandas as pd
+
 BASE_DIR = Path(__file__).parent
+
 
 def save_model(model: dict, name: str, dir: str = BASE_DIR / "src" / "models/"):
     # print(dir.resolve().exists())
@@ -12,8 +12,15 @@ def save_model(model: dict, name: str, dir: str = BASE_DIR / "src" / "models/"):
     with open(file, "wb") as f:
         dill.dump(model, f)
 
+
 def load_model(name: str, dir: str = BASE_DIR / "src" / "models/"):
     file = Path(dir) / f"{name}.pkl"
     with open(file, "rb") as f:
         model = dill.load(f)
     return model
+
+
+def raise_error(condition, message):
+    """Raise an error if the condition is True."""
+    if condition:
+        raise ValueError(message)
